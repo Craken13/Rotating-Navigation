f24a2bb821c665f4a04cf0cd439f57b1d2ea1d51
Craken13 <49389973+Craken13@users.noreply.github.com>
2021-03-09 22:08:00 +0200

    Merge Changes

--- script.js
+++ script.js
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 const open = document.getElementById('open')
 const close = document.getElementById('close')
 const container = document.querySelector('.container')
 
 open.addEventListener('click', () => container.classList.add('show-nav'))
 
-=======
-const open = document.getElementById('open')
-const close = document.getElementById('close')
-const container = document.querySelector('.container')
-
-open.addEventListener('click', () => container.classList.add('show-nav'))
-
->>>>>>> f4c6d887d8c453743149dd9a78f8bd662df01dcb
 close.addEventListener('click', () => container.classList.remove('show-nav'))
